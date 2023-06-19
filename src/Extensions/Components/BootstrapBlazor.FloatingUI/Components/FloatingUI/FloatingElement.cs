99e30575260a0404841a606dc3a540dd7bfb0ec0
Argo-AscioTech <argo@live.ca>
2023-06-19 16:37:42 +0800

    feat: 生成组件

---
+++ src/Extensions/Components/BootstrapBlazor.FloatingUI/Components/FloatingUI/FloatingElement.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 悬浮组件
+/// </summary>
+public class FloatingElement : IdComponentBase
+{
+    /// <summary>
+    /// 获得/设置 RenderFragment 实例
+    /// </summary>
+    [Parameter]
+#if NET6_0_OR_GREATER
+    [EditorRequired]
+#endif
+    [NotNull]
+    public RenderFragment? ChildContent { get; set; }
+
+    [CascadingParameter]
+    private FloatingUI? Parent { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        Parent?.RegisterFloatElement(this);
+    }
+}
---
+++ src/Extensions/Components/BootstrapBlazor.FloatingUI/Components/FloatingUI/FloatingUI.razor.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+
+namespace BootstrapBlazor.Components;
+
+public partial class FloatingUI
+{
+    private string? ClassString => CssBuilder.Default("bb-float-ui")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+
+    /// <summary>
+    /// 获得/设置 RenderFragment 实例
+    /// </summary>
+    [Parameter]
+#if NET6_0_OR_GREATER
+    [EditorRequired]
+#endif
+    [NotNull]
+    public RenderFragment? ChildContent { get; set; }
+
+    private FloatingElement? Element { get; set; }
+
+    private bool IsRendered { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="firstRender"></param>
+    protected override void OnAfterRender(bool firstRender)
+    {
+        base.OnAfterRender(firstRender);
+
+        if (firstRender)
+        {
+            IsRendered = true;
+            StateHasChanged();
+        }
+    }
+
+    /// <summary>
+    /// 子组件实例化悬浮组件方法
+    /// </summary>
+    /// <param name="element"></param>
+    internal void RegisterFloatElement(FloatingElement element) => Element = element;
+
+    private RenderFragment RenderFloatingElement() => builder =>
+    {
+        builder.OpenElement(0, "div");
+        builder.AddAttribute(1, "class", "bb-float-dropdown");
+        if (Element != null)
+        {
+            builder.AddContent(2, Element.ChildContent);
+        }
+        builder.CloseElement();
+    };
+}
